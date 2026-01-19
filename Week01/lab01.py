7250f2fc84109833a4d5068cfc28efbda9220413
affan.telek <affan.telek@gmail.com>
2026-01-18 19:05:10 -0500

    add lab 01 with all answers

--- Week01/lab01.py
+++ Week01/lab01.py
@@ -4,3 +4,12 @@
 
 # Question 2
 my_array = [1, 4, 7, 9]
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e = ( a - ((b ** c) // d)) + ( a % c)
